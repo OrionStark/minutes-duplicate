50d795668e32dca8dae03ac07f3fd7fd0918cbe4
Robby Muhammad Nst <robbybellamy6@gmail.com>
2017-10-16 05:12:10 +0700

    Redesign register page

--- app/src/main/java/com/example/orion_stark/minutes_duplicate/Register.java
+++ app/src/main/java/com/example/orion_stark/minutes_duplicate/Register.java
@@ -8,11 +8,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class Register extends AppCompatActivity {
 
     private Toolbar toolbar;
     private Button signup_btn;
+    private EditText firstName;
+    private EditText lastName;
+    private EditText email;
+    private EditText password;
+    private EditText phoneNumber;
+    private EditText password_confirm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,12 +41,18 @@ public class Register extends AppCompatActivity {
         });
     }
     private void setProps(){
-        this.signup_btn = (Button)findViewById(R.id.button_signup);
-        this.signup_btn.setTypeface(Typeface.createFromAsset(getAssets(), "Font_Pack/Poppins-Medium.ttf"));
-        this.signup_btn.setOnClickListener(new View.OnClickListener() {
+        this.signup_btn = (Button)findViewById(R.id.signup_btn);
+        this.firstName = (EditText)findViewById(R.id.first_name_field);
+        this.lastName = (EditText)findViewById(R.id.last_name_field);
+        this.email = (EditText)findViewById(R.id.email_field);
+        this.phoneNumber = (EditText)findViewById(R.id.phone_number);
+        this.password = (EditText)findViewById(R.id.password_field);
+        this.password_confirm = (EditText)findViewById(R.id.retype_password_field);
+
+        this.signup_btn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                //TO_DO
+                //
             }
         });
     }
