cd51433d373f525b977fc87b7e6700431dc38df3
ianstevenz <iansteven98@gmail.com>
2017-12-01 15:42:09 +0700

    Card Item

---
+++ app/src/main/java/com/example/orion_stark/minutes_duplicate/Home_Page.java
@@ -0,0 +1,13 @@
+package com.example.orion_stark.minutes_duplicate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Home_Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_page);
+    }
+}
---
+++ app/src/main/java/com/example/orion_stark/minutes_duplicate/controllers/RecyclerViewHolder.java
@@ -0,0 +1,8 @@
+package com.example.orion_stark.minutes_duplicate.controllers;
+
+/**
+ * Created by evan on 28/11/17.
+ */
+
+public class RecyclerViewHolder {
+}
